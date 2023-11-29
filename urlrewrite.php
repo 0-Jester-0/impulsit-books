1a3aa2dd0d327c00dda657fe0a674b0ae1b81d35
0-Jester-0 <rom.mart1202@gmail.com>
2023-11-29 04:51:04 +0300

    Добавлено правило в urlrewrite

--- urlrewrite.php
+++ urlrewrite.php
@@ -24,4 +24,12 @@ $arUrlRewrite=array (
     'PATH' => '/news/index.php',
     'SORT' => 100,
   ),
+  3 => 
+  array (
+    'CONDITION' => '#^#',
+    'RULE' => '',
+    'ID' => 'bitrix:main.pagenavigation',
+    'PATH' => '/books/index.php',
+    'SORT' => 100,
+  ),
 );
